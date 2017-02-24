97108d595a20c9d4a408b83e82006918bbe11eb4
Marcel Amsler <marcel.amsler@gmail.com>
2017-02-24 23:26:13 +0100

    updated userrole

--- app/controllers/test_websites_controller.rb
+++ app/controllers/test_websites_controller.rb
@@ -1,3 +1,5 @@
+require 'selenium-webdriver'
+
 class TestWebsitesController < ApplicationController
 
   def new
@@ -7,6 +9,17 @@ class TestWebsitesController < ApplicationController
   def create
     url = params[:url]
     puts url
+
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to url
+
+    element = driver.find_element(:name, 'q')
+    element.send_keys "Hello WebDriver!"
+    element.submit
+
+    puts driver.title
+
+    driver.quit
     render :new
   end
 
